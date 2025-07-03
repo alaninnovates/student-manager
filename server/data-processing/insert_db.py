9d3ceec1da812db7a10eee0c41ef0e4b87fbce28
alaninnovates <alanlichen1@gmail.com>
2025-07-03 00:04:23 -0400

    Add script to insert students to db

---
+++ server/data-processing/insert_db.py
@@ -0,0 +1,57 @@
+from supabase import create_client
+from dotenv import load_dotenv
+import pandas as pd
+import os
+import json
+from datetime import datetime
+
+load_dotenv()
+
+client = create_client(
+    os.getenv("SUPABASE_URL"),
+    os.getenv("SUPABASE_SERVICE_KEY")
+)
+
+files = os.listdir('out')
+
+def insert_classes():
+    to_write = []
+    for file in files:
+        with open(f'out/{file}', 'r') as f:
+            data = json.load(f)
+            class_name = data['class_name']
+            class_dates = data['class_dates']
+            to_write.append({
+                'name': class_name,
+                'dates': [
+                    datetime.fromtimestamp(date).strftime('%Y-%m-%d') for date in class_dates
+                ],
+            })
+    client.table('classes').insert(to_write).execute()
+
+def insert_students():
+    student_metadata = []
+    for file in files:
+        with open(f'out/{file}', 'r') as f:
+            data = json.load(f)
+            students = data['students']
+            for student in students:
+                if student['name'] not in [s['name'] for s in student_metadata]:
+                    student_metadata.append({
+                        'name': student['name'],
+                        'grade': student['grade'] if student['grade'] not in (None, float('nan')) else 0,
+                        'parent_cells': [str(student['parent_cells'])] if not pd.isna(student['parent_cells']) else [],
+                        'parent_emails': [student['parent_email']] if not pd.isna(student['parent_email']) else [],
+                    })
+                else:
+                    for s in student_metadata:
+                        if s['name'] == student['name']:
+                            if str(student['parent_cells']) not in s['parent_cells'] and not pd.isna(student['parent_cells']):
+                                s['parent_cells'].append(str(student['parent_cells']))
+                            if student['parent_email'] not in s['parent_emails'] and not pd.isna(student['parent_email']):
+                                s['parent_emails'].append(student['parent_email'])
+                            break
+    client.table('students').insert(student_metadata).execute()
+
+classes = json.load(open('classes.json', 'r'))
+students = json.load(open('students.json', 'r'))
