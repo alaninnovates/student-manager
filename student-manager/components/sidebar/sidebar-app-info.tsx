a5a453a9dca163b9e5caaa2a76155cc53a59a21c
alaninnovates <alanlichen1@gmail.com>
2025-07-04 00:37:36 -0400

    Change icon to school

--- student-manager/components/sidebar/sidebar-app-info.tsx
+++ student-manager/components/sidebar/sidebar-app-info.tsx
@@ -1,4 +1,4 @@
-import { GalleryVerticalEnd } from 'lucide-react';
+import { School2 } from 'lucide-react';
 import {
     SidebarMenu,
     SidebarMenuButton,
@@ -14,7 +14,7 @@ export const SidebarAppInfo = () => {
                     className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                 >
                     <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                        <GalleryVerticalEnd className="size-4" />
+                        <School2 className="size-4" />
                     </div>
                     <div className="flex flex-col gap-0.5 leading-none">
                         <span className="font-medium">Student Manager</span>
